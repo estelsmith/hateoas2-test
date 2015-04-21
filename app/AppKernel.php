b41a8ed2eb76b15e4666f476e7be01d324a5bcf8
Estel Smith <estel.smith@gmail.com>
2015-04-21 14:47:57 -0400

    Added DoctrineFixturesBundle to the application.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -11,6 +11,7 @@ class AppKernel extends Kernel
             // Symfony and 3rd party bundles
             new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
