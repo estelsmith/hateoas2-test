e2207b2c7eb924f0401995cafbc4c2d043cc832f
Estel Smith <estel.smith@gmail.com>
2015-04-21 14:52:30 -0400

    Added rudimentary user fixture.

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadUserData implements FixtureInterface, OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $users = [
+            [
+                'username' => 'test1',
+                'password' => 'test1'
+            ],
+            [
+                'username' => 'test2',
+                'password' => 'test2'
+            ],
+            [
+                'username' => 'test3',
+                'password' => 'test3'
+            ]
+        ];
+
+        foreach ($users as $userData) {
+            $user = (new User())
+                ->setUsername($userData['username'])
+                ->setPassword($userData['password'])
+            ;
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 0;
+    }
+}
