faa85783123be522f99d86686a075e2c6c03dce7
Estel Smith <estel.smith@gmail.com>
2015-04-21 19:05:21 -0400

    Added custom serialization to the User entity.

---
+++ src/ApiBundle/DependencyInjection/CompilerPass/SerializerMetadataCompilerPass.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ApiBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class SerializerMetadataCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition('jms_serializer.metadata.file_locator');
+        $metadataDirectories = $definition->getArguments()[0];
+
+        $metadataDirectories['AppBundle'] = realpath(__DIR__ . '/../..') . '/Resources/config/serializer/AppBundle';
+
+        $definition->replaceArgument(0, $metadataDirectories);
+    }
+}
