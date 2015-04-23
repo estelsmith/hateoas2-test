8ebcc0adb0f8dcf5990bce31e595118d77607000
Estel Smith <estel.smith@gmail.com>
2015-04-23 11:13:48 -0400

    Added a serialization event subscriber to add resource types.

---
+++ src/ApiBundle/Serializer/Subscriber/ResourceTypeSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ApiBundle\Serializer\Subscriber;
+
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\GenericSerializationVisitor;
+
+class ResourceTypeSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var array
+     */
+    private $registeredTypes;
+
+    /**
+     * @param array $registeredTypes
+     */
+    function __construct(array $registeredTypes = [])
+    {
+        $this->registeredTypes = $registeredTypes;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            [
+                'event' => Events::POST_SERIALIZE,
+                'method' => 'onPostSerialize'
+            ]
+        ];
+    }
+
+    public function onPostSerialize(ObjectEvent $event)
+    {
+        $registeredTypes = $this->registeredTypes;
+        $type = $event->getType()['name'];
+
+        if (array_key_exists($type, $registeredTypes)) {
+            /** @var GenericSerializationVisitor $visitor */
+            $visitor = $event->getVisitor();
+            $visitor->addData('type', $registeredTypes[$type]);
+        }
+    }
+}
