c5f3545143fc635fa41010d490ffb8966e9bdb87
Estel Smith <estel.smith@gmail.com>
2015-04-24 17:49:48 -0400

    Added collection response helper to the json-api base controller.

--- src/ApiBundle/Controller/JsonApiController.php
+++ src/ApiBundle/Controller/JsonApiController.php
@@ -2,6 +2,7 @@
 
 namespace ApiBundle\Controller;
 
+use ApiBundle\Representation\CollectionRepresentation;
 use ApiBundle\Representation\ResourceRepresentation;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -22,9 +23,32 @@ class JsonApiController extends Controller
         $this->serializer = $serializer;
     }
 
+    /**
+     * @param mixed $resource
+     * @param string|null $route
+     * @param array $parameters
+     * @return Response
+     */
     public function createResourceResponse($resource, $route = null, array $parameters = [])
     {
         $representation = new ResourceRepresentation($resource, $route, $parameters);
+
+        return new Response(
+            $this->serializer->serialize($representation, 'json'),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/json']
+        );
+    }
+
+    /**
+     * @param mixed $resources
+     * @param string $route
+     * @param array $parameters
+     * @return Response
+     */
+    public function createCollectionResponse($resources, $route, array $parameters = [])
+    {
+        $representation = new CollectionRepresentation($resources, $route, $parameters);
 
         return new Response(
             $this->serializer->serialize($representation, 'json'),
