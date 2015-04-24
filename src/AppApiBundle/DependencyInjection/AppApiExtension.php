e00f38c3f75a4df210962ca1b44595143d7e9b4f
Estel Smith <estel.smith@gmail.com>
2015-04-24 17:36:29 -0400

    Made application's api mapping available via mapping.yml.

--- src/AppApiBundle/DependencyInjection/AppApiExtension.php
+++ src/AppApiBundle/DependencyInjection/AppApiExtension.php
@@ -4,15 +4,27 @@ namespace AppApiBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Parser;
 
-class AppApiExtension extends Extension
+class AppApiExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $config, ContainerBuilder $container)
     {
         $fileLocator = new FileLocator([__DIR__ . '/../Resources/config']);
         $yamlLoader = new YamlFileLoader($container, $fileLocator);
         $yamlLoader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $fileLocator = new FileLocator([__DIR__ . '/../Resources/config']);
+
+        $parser = new Parser();
+        $mappingConfiguration = $parser->parse(file_get_contents($fileLocator->locate('mapping.yml')));
+
+        $container->prependExtensionConfig('api', $mappingConfiguration);
     }
 }
