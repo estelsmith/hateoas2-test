9478c95242c984652df84f66887c73b11b2d9e2c
Estel Smith <estel.smith@gmail.com>
2015-04-24 17:50:36 -0400

    Re-added the ability to fetch all users.

--- src/AppApiBundle/Controller/UsersController.php
+++ src/AppApiBundle/Controller/UsersController.php
@@ -3,10 +3,28 @@
 namespace AppApiBundle\Controller;
 
 use ApiBundle\Controller\JsonApiController;
+use AppBundle\Entity\Repository\UserRepository;
 use AppBundle\Entity\User;
+use JMS\Serializer\SerializerInterface;
 
 class UsersController extends JsonApiController
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @param SerializerInterface $serializer
+     * @param UserRepository $userRepository
+     */
+    public function __construct(SerializerInterface $serializer, UserRepository $userRepository)
+    {
+        parent::__construct($serializer);
+
+        $this->userRepository = $userRepository;
+    }
+
     public function getOneAction(User $user)
     {
         return $this->createResourceResponse($user);
@@ -14,6 +32,6 @@ class UsersController extends JsonApiController
 
     public function getAllAction()
     {
-
+        return $this->createCollectionResponse($this->userRepository->findAll(), 'app_users_get_all');
     }
 }
