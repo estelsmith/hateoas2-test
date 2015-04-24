d49bc1edacadfc27f920ceb3e41a6201be20699a
Estel Smith <estel.smith@gmail.com>
2015-04-24 16:55:35 -0400

    Created a generic resource response helper method.

--- src/ApiBundle/Controller/JsonApiController.php
+++ src/ApiBundle/Controller/JsonApiController.php
@@ -2,9 +2,34 @@
 
 namespace ApiBundle\Controller;
 
+use ApiBundle\Representation\ResourceRepresentation;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class JsonApiController extends Controller
 {
+    /**
+     * @var SerializerInterface
+     */
+    protected $serializer;
 
+    /**
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function createResourceResponse($resource, $route = null, array $parameters = [])
+    {
+        $representation = new ResourceRepresentation($resource, $route, $parameters);
+
+        return new Response(
+            $this->serializer->serialize($representation, 'json'),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/json']
+        );
+    }
 }
--- src/AppApiBundle/Controller/UsersController.php
+++ src/AppApiBundle/Controller/UsersController.php
@@ -3,36 +3,13 @@
 namespace AppApiBundle\Controller;
 
 use ApiBundle\Controller\JsonApiController;
-use ApiBundle\Representation\ResourceRepresentation;
 use AppBundle\Entity\User;
-use JMS\Serializer\SerializerInterface;
-use Symfony\Component\HttpFoundation\Response;
 
 class UsersController extends JsonApiController
 {
-    /**
-     * @var SerializerInterface
-     */
-    private $serializer;
-
-    /**
-     * @param SerializerInterface $serializer
-     */
-    public function __construct(SerializerInterface $serializer)
-    {
-        $this->serializer = $serializer;
-    }
-
     public function getOneAction(User $user)
     {
-//        return $this->createResourceResponse($user);
-        $representation = new ResourceRepresentation($user);
-
-        return new Response(
-            $this->serializer->serialize($representation, 'json'),
-            Response::HTTP_OK,
-            ['Content-Type' => 'application/json']
-        );
+        return $this->createResourceResponse($user);
     }
 
     public function getAllAction()
