aea1a83feafa1737b18b50c5f52155f1bf51e511
Estel Smith <estel.smith@gmail.com>
2015-04-24 16:34:14 -0400

    Added an API configuration object.

---
+++ src/ApiBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $root = $treeBuilder->root('api');
+
+        $root
+            ->children()
+                ->arrayNode('mapping')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('resource_types')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('class')->end()
+                                    ->scalarNode('type')->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('relations')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->arrayNode('self')
+                                    ->prototype('array')
+                                        ->children()
+                                            ->scalarNode('class')->end()
+                                            ->scalarNode('route')->end()
+                                            ->arrayNode('parameters')
+                                                ->prototype('scalar')->end()
+                                            ->end()
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
